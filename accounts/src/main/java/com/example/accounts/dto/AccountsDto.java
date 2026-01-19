c19e838380eacd14307e1581bead29eb1f16d982
disha <dishanayak395@gmail.com>
2026-01-19 21:23:33 +0530

    add input data validations

--- accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
@@ -1,13 +1,20 @@
 package com.example.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
 @Data
 public class AccountsDto {
+
+    @NotEmpty(message = "AccountNumber cannot be empty")
+    @Pattern(regexp = "($|[0-9]{10})", message = "Account Number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType cannot be a null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
     private String branchAddress;
 }
