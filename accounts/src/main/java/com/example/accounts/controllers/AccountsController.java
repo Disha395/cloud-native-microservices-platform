15f55103fa5fb14ffc7680fce79496962ea9952c
disha <dishanayak395@gmail.com>
2026-02-06 07:40:28 +0530

    add redis rate limiter

--- accounts/src/main/java/com/example/accounts/controllers/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controllers/AccountsController.java
@@ -5,6 +5,7 @@ import com.example.accounts.dto.CustomerDto;
 import com.example.accounts.dto.ErrorResponseDto;
 import com.example.accounts.dto.ResponseDto;
 import com.example.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -25,6 +26,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import static org.apache.commons.lang3.SystemUtils.JAVA_HOME;
+
 /*
 @Tag(
         name = "CRUD REST APIs for Account Microservice",
@@ -238,11 +242,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    //Method Signature should be same
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
